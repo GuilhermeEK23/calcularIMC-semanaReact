f7be787c3f82357b536dde6555a089de290771dc
GuilhermeEK <guilhermeelizeu23@gmail.com>
2024-07-23 22:56:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const [peso, setPeso] = useState('')
+  const [altura, setAltura] = useState('')
+
+  const [mensagem, setMensagem] = useState('')
+  
+  function calcularIMC(){
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+
+    if(imc < 18.5){
+      setMensagem('Você está abaixo do peso! Seu imc: ' + imc.toFixed(2))
+    } else if(imc >= 18.5 && imc <= 24.9){
+      setMensagem('Peso ideal! Seu imc: ' + imc.toFixed(2))
+    } else if(imc >= 25 && imc <= 29.9) {
+      setMensagem('Você está um pouco acima do peso! Seu imc: ' + imc.toFixed(2))
+    } else if(imc >= 30) {
+      setMensagem('Cuidado, sobrepeso! Seu imc: ' + imc.toFixed(2))
+    }
+  }
+
+  return(
+    <div className="app">
+      <h1>Calculadora IMC</h1>
+      <span>Vamos calcular o seu IMC</span>
+
+      <div className="area-input">
+        <input 
+          type="text" 
+          placeholder="Peso em (kg) ex: 90"
+          value={peso}
+          onChange={ (e) => setPeso(e.target.value)}
+        />
+        <input 
+          type="text"
+          placeholder="Altura em (cm) ex: 175"
+          value={altura}
+          onChange={ (e) => setAltura(e.target.value)}
+        />
+
+        <button onClick={calcularIMC}>
+          calcular
+        </button>
+      </div>
+
+      <h2>{mensagem}</h2>
     </div>
-  );
+  )
 }
-
-export default App;
